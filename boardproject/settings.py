f4718ee58d6b963a676b8b6a48a06f8ce04874e3
damin0320 <damin0320@gmail.com>
2021-10-19 16:18:55 +0900

    CHNAGE : SECRET_KEY, DATABASES hide

--- boardproject/settings.py
+++ boardproject/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from my_settings import SECRET_KEY, DATABASES
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q5d-r9cfst_m7*hd-c$_n6$y-qan!mw$*2r=09zz7z-((3!hts'
+SECRET_KEY = SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,12 +77,7 @@ WSGI_APPLICATION = 'boardproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = DATABASES
 
 
 # Password validation
---
+++ my_settings.py
@@ -0,0 +1,8 @@
+SECRET_KEY = 'django-insecure-q5d-r9cfst_m7*hd-c$_n6$y-qan!mw$*2r=09zz7z-((3!hts'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'boardproject',
+    }
+}
